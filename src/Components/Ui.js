b8fd0b5043c85cac4fac116ac2679991934d15d5
Sagar Roy <sagarroy761997@gmail.com>
2023-01-09 17:53:02 +0530

    sunrise added

--- src/Components/Ui.js
+++ src/Components/Ui.js
@@ -10,9 +10,11 @@ import IconButton from "@mui/material/IconButton";
 import AddBoxIcon from "@mui/icons-material/AddBox";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import { styled } from "@mui/material/styles";
-import NearMeOutlinedIcon from '@mui/icons-material/NearMeOutlined';
-import LightModeIcon from '@mui/icons-material/LightMode';
-import BedtimeIcon from '@mui/icons-material/Bedtime';
+import NearMeOutlinedIcon from "@mui/icons-material/NearMeOutlined";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import BedtimeIcon from "@mui/icons-material/Bedtime";
+import WbSunnyIcon from '@mui/icons-material/WbSunny';
+
 
 import Chart from "./Chart";
 import Rain from "./Rain";
@@ -22,6 +24,7 @@ import FeelsLike from "./FeelsLike";
 
 const useStyles = makeStyles({
   root: {
+    fontSize: '1.1rem',
     backgroundColor: "#b5d8fe",
     display: "flex",
     justifyContent: "center",
@@ -38,9 +41,9 @@ const useStyles = makeStyles({
     backgroundColor: "#5d9ce6 !important",
   },
   data: {
-    padding: "2% 0 2% 2%",
+    padding: "2% 2% 2% 2%",
     display: "flex",
-    flexDirection:'column',
+    flexDirection: "column",
     // justifyContent:'space-between',
     width: "30%",
     height: "100%",
@@ -115,8 +118,8 @@ const useStyles = makeStyles({
     justifyContent: "center",
     alignItems: "center",
   },
-  temp:{
-    fontSize:'3em !important'
+  temp: {
+    fontSize: "3em !important",
   },
 });
 
@@ -135,62 +138,14 @@ const Ui = () => {
   const [pressure, setPressure] = useContext(LocationContext).pressure;
   const [feelsLike, setFeelsLike] = useContext(LocationContext).feelsLike;
   const [humidity, setHumidity] = useContext(LocationContext).humidity;
-  const [checked, setChecked] = useState('false')
-  const [day, setDay] = useState('');
-  const [night, setNight] = useState('')
+  const [checked, setChecked] = useState("false");
+  const [day, setDay] = useState("");
+  const [night, setNight] = useState("");
   // console.log(useContext(LocationContext));
   const date = new Date();
   useEffect(() => {
     getLocation();
   }, []);
-  // const MaterialUISwitch = styled(Switch)(({ theme }) => ({
-  //   width: 62,
-  //   height: 34,
-  //   padding: 7,
-  //   "& .MuiSwitch-switchBase": {
-  //     margin: 1,
-  //     padding: 0,
-  //     transform: "translateX(6px)",
-  //     "&.Mui-checked": {
-  //       color: "#fff",
-  //       transform: "translateX(22px)",
-  //       "& .MuiSwitch-thumb:before": {
-  //         backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
-  //           "#fff"
-  //         )}" d="M4.2 2.5l-.7 1.8-1.8.7 1.8.7.7 1.8.6-1.8L6.7 5l-1.9-.7-.6-1.8zm15 8.3a6.7 6.7 0 11-6.6-6.6 5.8 5.8 0 006.6 6.6z"/></svg>')`,
-  //       },
-  //       "& + .MuiSwitch-track": {
-  //         opacity: 1,
-  //         backgroundColor:
-  //           theme.palette.mode === "dark" ? "#8796A5" : "#aab4be",
-  //       },
-  //     },
-  //   },
-  //   "& .MuiSwitch-thumb": {
-  //     backgroundColor: theme.palette.mode === "dark" ? "#003892" : "#001e3c",
-  //     width: 32,
-  //     height: 32,
-  //     "&:before": {
-  //       content: "''",
-  //       position: "absolute",
-  //       width: "100%",
-  //       height: "100%",
-  //       left: 0,
-  //       top: 0,
-  //       backgroundRepeat: "no-repeat",
-  //       backgroundPosition: "center",
-  //       backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
-  //         "#fff"
-  //       )}" d="M9.305 1.667V3.75h1.389V1.667h-1.39zm-4.707 1.95l-.982.982L5.09 6.072l.982-.982-1.473-1.473zm10.802 0L13.927 5.09l.982.982 1.473-1.473-.982-.982zM10 5.139a4.872 4.872 0 00-4.862 4.86A4.872 4.872 0 0010 14.862 4.872 4.872 0 0014.86 10 4.872 4.872 0 0010 5.139zm0 1.389A3.462 3.462 0 0113.471 10a3.462 3.462 0 01-3.473 3.472A3.462 3.462 0 016.527 10 3.462 3.462 0 0110 6.528zM1.665 9.305v1.39h2.083v-1.39H1.666zm14.583 0v1.39h2.084v-1.39h-2.084zM5.09 13.928L3.616 15.4l.982.982 1.473-1.473-.982-.982zm9.82 0l-.982.982 1.473 1.473.982-.982-1.473-1.473zM9.305 16.25v2.083h1.389V16.25h-1.39z"/></svg>')`,
-  //     },
-  //   },
-  //   "& .MuiSwitch-track": {
-  //     opacity: 1,
-  //     backgroundColor: theme.palette.mode === "dark" ? "#8796A5" : "#aab4be",
-  //     borderRadius: 20 / 2,
-  //   },
-  // }));
-
   const getLocation = () => {
     if ("geolocation" in navigator) {
       navigator.geolocation.getCurrentPosition(async (position) => {
@@ -201,10 +156,8 @@ const Ui = () => {
           .then((fetched) => {
             if (fetched.status === 200) {
               console.log(fetched?.data);
-              setWeatherData(fetched?.data);
               setMainTemp(fetched?.data?.main?.temp);
               setCountry(fetched?.data?.sys?.country);
-              setSky(fetched?.data?.weather?.[0]?.main);
               setArea(fetched?.data?.name);
               setWindSpeed(fetched?.data?.wind?.speed);
               setPressure(fetched?.data?.main?.pressure);
@@ -216,9 +169,17 @@ const Ui = () => {
               );
               setChecked(false);
               setUnitTemp(" °C");
-              // setDay(((Date(fetched?.data?.sys?.sunrise))).toString().toDateString());
-              // setDay(new Date(fetched?.data?.sys?.sunrise).toString())
-              // console.log(day)
+
+              setDay(
+                new Date(
+                  parseInt(fetched?.data?.sys?.sunrise) * 1000
+                ).toLocaleTimeString()
+              );
+              setNight(
+                new Date(
+                  parseInt(fetched?.data?.sys?.sunset) * 1000
+                ).toLocaleTimeString()
+              );
             }
           });
         await axios
@@ -261,26 +222,43 @@ const Ui = () => {
               </IconButton>
               <Box>
                 {"°C"}
-                <Switch onChange={()=>{conversion();setChecked(!checked);}} checked={checked}/>
+                <Switch
+                  onChange={() => {
+                    conversion();
+                    setChecked(!checked);
+                  }}
+                  checked={checked}
+                />
                 {"°F"}
-                {/* <FormControlLabel
-                  control={<MaterialUISwitch sx={{ m: 1 }} onChange={conversion} checked={checked}/>}
-                  // label="MUI switch"
-                /> */}
               </Box>
             </Box>
-            <Box className={classes.location}>
-              <NearMeOutlinedIcon fontSize="small"/>
-              <Box>
-                {`${area}, ${country}`}
+            <Box sx={{display: 'flex', justifyContent: 'space-between'}}>
+              <Box >
+                <Box className={classes.location}>
+                  <NearMeOutlinedIcon fontSize="small" />
+                  <Box>{`${area}, ${country}`}</Box>
+                </Box>
+                <Box>{date.toDateString()}</Box>
               </Box>
-              
+              <Box sx={{display: 'flex', flexDirection: 'column' , justifyContent: 'right', alignItems: 'center'}}>
+              <Box sx={{display: 'flex', justifyContent:'center', alignItems: 'center'}}>
+                <Box><WbSunnyIcon fontSize="small"/></Box>
+                <Box>{day}</Box>
+                
+              </Box>
+              <Box sx={{display: 'flex', justifyContent:'center', alignItems: 'center'}}>
+                <Box><BedtimeIcon fontSize="small"/></Box>
+                <Box>{night}</Box>
+                
+              </Box>
+              </Box>
             </Box>
-            <Box>{date.toDateString()}</Box>
-            {day}
+
             <Box className={classes.description}>
               <Box>
-                <Typography className={classes.temp}>{`${mainTemp} ${unitTemp}`}</Typography>
+                <Typography
+                  className={classes.temp}
+                >{`${mainTemp} ${unitTemp}`}</Typography>
               </Box>
               <Box>
                 <img
